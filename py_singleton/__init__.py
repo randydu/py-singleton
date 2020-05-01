d216d9881ba8448eecf925f617501a949ae38f5b
randy <randydu@gmail.com>
2020-04-30 22:32:41 -0700

    bump version to v0.9.1;

--- py_singleton/__init__.py
+++ py_singleton/__init__.py
@@ -2,7 +2,7 @@
     function utilities
 """
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 
 
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="py-singleton", 
-    version="0.9.0",
+    version="0.9.1",
     description = "singleton pattern for python 2 & 3",
     url="https://github.com/randydu/py-singleton.git",
     author="Randy Du",
