07f97b223cada8907d226ba896f369a26eae63ef
randy <randydu@gmail.com>
2020-04-30 22:21:08 -0700

    prepare publish to pypi;

--- setup.py
+++ setup.py
@@ -6,5 +6,18 @@ setup(name="py-singleton",
     url="https://github.com/randydu/py-singleton.git",
     author="Randy Du",
     author_email="randydu@gmail.com",
-    packages=["py_singleton"]
+    packages=["py_singleton"],
+    keywords=["singleton"],
+    license="MIT",
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers', 
+    'Operating System :: OS Independent',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+  ],
+
 )
